747274a9410542869eafdce44ed0d931b8096d8f
jomingyun <jmg97@kakao.com>
2023-07-21 09:03:35 +0900

    20542 test commit

---
+++ mingyun/mingyun/src/week1_bj_20542_받아쓰기.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class week1_bj_20542_받아쓰기 {
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream("res/input_bj_20542.txt"));
+    }
+}
