654baa583c0c8b9699e6f5d3b6e4fa0e62ccf739
gkstmf1403 <gkstmf1403@naver.com>
2023-07-24 18:01:22 +0900

    commit

--- hanseul/src/week1_bj_20542.java
+++ hanseul/src/week1_bj_20542.java
@@ -1,19 +1,29 @@
 import java.util.*;
 import java.io.*;
 
+
+class character {
+    int cnt;
+    char ch;
+}
 public class week1_bj_20542 {
 
+    static int[] arr;
+    static int[] arr2;
     static int n;
     static int m;
     static String nStr;
     static String mStr;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        nStr = br.readLine();
+        mStr = br.readLine();
+
 
     }
 }
