5350834fc29fa511848360644ddcc8998dee1110
gkstmf1403 <gkstmf1403@naver.com>
2023-07-26 14:56:13 +0900

    bj 20542 done

--- hanseul/src/week1_bj_20542.java
+++ hanseul/src/week1_bj_20542.java
@@ -1,15 +1,9 @@
 import java.util.*;
 import java.io.*;
 
-
-class character {
-    int cnt;
-    char ch;
-}
 public class week1_bj_20542 {
 
-    static int[] arr;
-    static int[] arr2;
+    static int[][] arr;
     static int n;
     static int m;
     static String nStr;
@@ -24,6 +18,28 @@ public class week1_bj_20542 {
         nStr = br.readLine();
         mStr = br.readLine();
 
+        arr = new int[n + 1][m + 1];
+        for (int i=1;i<=n;i++)
+            arr[i][0] = i;
+        for (int i=1;i<=m;i++)
+            arr[0][i] = i;
 
+        for (int i=1;i <= n; i++)
+        {
+            for (int j=1;j <= m;j++)
+            {
+                int min = (arr[i - 1][j - 1] > arr[i - 1][j]) ? arr[i - 1][j] : arr[i - 1][j - 1];
+                min = (min > arr[i][j - 1]) ? arr[i][j - 1] : min;
+
+                boolean bool;
+                char ch = nStr.charAt(i - 1);
+                char ch1 = mStr.charAt(j - 1);
+//                if (nStr.charAt(i - 1))
+
+                if (arr[i][0] == arr[0][j]) arr[i][j] = min;
+                else arr[i][j] = min + 1;
+            }
+        }
+        System.out.println(arr[n][m]);
     }
 }
