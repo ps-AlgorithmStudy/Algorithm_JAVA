ee4cdf6dd92a2f4efdf26359d6312567e02d0a52
dig04214 <dig04214@naver.com>
2023-07-27 03:56:12 +0900

    1912, 20542, 25215

--- byeongchang/src/week1_bj_20542.java
+++ byeongchang/src/week1_bj_20542.java
@@ -1,39 +1,61 @@
 import java.util.*;
 import java.io.*;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-
-class Main {
-    
-}
 
 public class week1_bj_20542 {
     
-    
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("C:\\Users\\User\\Documents\\dev\\java project\\algorithm-study-input\\bj_20542.txt"));
+    	/*입력파일 설정*/
+        //System.setIn(new FileInputStream("res\\bj_20542.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int TestCase = Integer.parseInt(st.nextToken());
-        for (int i = 1; i <= TestCase; i++) {
-            System.out.print("Case #" + i + ": ");
-            week1_bj_20542.solution(br, st);
-        }
+        StringTokenizer st = null;
+        /* swea 방식으로 여러 테케 돌릴 때 */
+        //TestCases(br, st);
+        
+        //st = new StringTokenizer(br.readLine());
+        
+        /* 단일 테케 돌릴 때 */
+        solution(br, st);
+        
     }
 
     public static void solution(BufferedReader br, StringTokenizer st) throws Exception {
         st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int result = 0;
-        String SY = br.readLine();
-        String answer = br.readLine();
-
-
-        System.out.println(sb.toString());
+        int N = Integer.parseInt(st.nextToken()); // 승연 답안 길이
+        int M = Integer.parseInt(st.nextToken()); // 정답 길이
+        int result = 0;  // 변경 횟수
+        String SY = br.readLine();  // 승연 답안
+        String answer = br.readLine();  // 답안
+        result = editDistance(SY.toCharArray(), answer.toCharArray());
+        System.out.println(result);
+    }
+    
+    public static int editDistance(char[] sy, char[] ans) {
+    	int[][] ed = new int[sy.length+1][ans.length+1];
+    	for(int i=0;i<=sy.length;i++) ed[i][0] = i;
+    	for(int i=0;i<=ans.length;i++) ed[0][i] = i;
+    	for(int i=0;i<sy.length;i++) {
+    		for(int j=0;j<ans.length;j++) {
+    			if(checkEqual(sy[i], ans[j])) ed[i+1][j+1] = ed[i][j];
+    			else ed[i+1][j+1] = Math.min(Math.min(ed[i][j+1], ed[i+1][j]), ed[i][j])+1;
+    		}
+    	}
+    	return ed[sy.length][ans.length];
+    }
+    // i == i,j,l   v == v,w
+    public static boolean checkEqual(char s, char a) {
+    	if (s == a) return true;
+    	else if (s == 'i' && (a == 'j' || a == 'l')) return true;
+    	else if (s == 'v' && a == 'w') return true;
+    	else return false;
+    }
+    
+    public static void TestCases(BufferedReader br, StringTokenizer st) throws Exception {
+    	st = new StringTokenizer(br.readLine());
+    	int TestCase = Integer.parseInt(st.nextToken());
+    	for (int i = 1; i <= TestCase; i++) {
+    		System.out.print("Case #" + i + ": ");
+    		week1_bj_20542.solution(br, st);
+    	}
     }
 }
 
