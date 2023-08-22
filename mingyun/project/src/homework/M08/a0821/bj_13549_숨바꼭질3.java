cebd95e2cc34120e1e2903c90bbbdf714bdb78f5
jmg97 <jmg97@kakao.com>
2023-08-22 11:06:06 +0900

    commit

--- mingyun/project/src/homework/M08/a0821/bj_13549_숨바꼭질3.java
+++ mingyun/project/src/homework/M08/a0821/bj_13549_숨바꼭질3.java
@@ -3,9 +3,11 @@ package homework.M08.a0821;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class bj_1697_숨바꼭질 {
+public class bj_13549_숨바꼭질3 {
     static class Obj {
         int pos, time;
         Obj(int a, int b){
@@ -20,29 +22,35 @@ public class bj_1697_숨바꼭질 {
 
         ArrayDeque<Obj> deque = new ArrayDeque<>();
 
-        boolean[] v = new boolean[2_0000_000];
-        v[n] = true;
+        int[] v = new int[1_000_000];
+        int[] pv = new int[1_000_000];
+        Arrays.fill(v,1_000_000);
+        v[n] = 0;
         deque.addLast(new Obj(n,0));
+        int r = 1_000_000;
         while (!deque.isEmpty()) {
             Obj now = deque.removeFirst();
             if (now.pos == m) {
-                System.out.println(now.time);
-                break;
+                r = Math.min(r, now.time);
             }
             else {
-                if ( !v[now.pos * 2]&&(now.pos * 2 - m == 1 || now.pos * 2 <= m)) {
-                    v[now.pos * 2] = true;
-                    deque.addLast(new Obj(now.pos*2, now.time+1));
+                if (now.pos * 2 <= 100000 && v[now.pos * 2] >= now.time &&(now.pos * 2 - m == 1 || now.pos * 2 <= m)) {
+                    if (pv[now.pos * 2] <= 10) {
+                        pv[now.pos * 2]++;
+                        v[now.pos * 2] = now.time;
+                        deque.addLast(new Obj(now.pos*2, now.time));
+                    }
                 }
-                if (!v[now.pos+1]) {
-                    v[now.pos+1] = true;
+                if (now.pos +1 <= 100000 && v[now.pos+1] > now.time) {
+                    v[now.pos+1] = now.time;
                     deque.addLast(new Obj(now.pos +1, now.time+1));
                 }
-                if (now.pos -1 >=0 && !v[now.pos-1]) {
-                    v[now.pos-1] = true;
+                if (now.pos -1 <= 100000 && now.pos -1 >=0 && v[now.pos-1] > now.time) {
+                    v[now.pos-1] = now.time;
                     deque.addLast(new Obj(now.pos -1, now.time+1));
                 }
             }
         }
+        System.out.println(r);
     }
 }
