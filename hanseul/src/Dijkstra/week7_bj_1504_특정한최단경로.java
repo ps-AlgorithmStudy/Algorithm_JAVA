3009106c7305880f0e03ab40746f76f94144d5eb
slcloe <gkstmf1403@naver.com>
2023-09-06 22:02:04 +0900

    0906

--- hanseul/src/Dijkstra/week7_bj_1504_특정한최단경로.java
+++ hanseul/src/Dijkstra/week7_bj_1504_특정한최단경로.java
@@ -5,44 +5,91 @@ import java.util.*;
 
 public class week7_bj_1504_특정한최단경로 {
 
-    static int N;
-    static int[][] arr;
-    static int[][] dp;
-
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
-
-    static int dfs(int x, int y, int weight){
-        if (dp[x][y] == 0)
-            dp[x][y] = weight;
-        if (dp[x][y] > weight)
-            dp[x][y] = weight;
-        else return 0;
+    static int N, E;
+    static int v1, v2;
+    static ArrayList<int[]>[] g;
 
 
-    }
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+    static int dijkstra(int start, int end){
+        if (start == end) return 0;
+        int[] minEdge = new int[N];
+        boolean[] v = new boolean[N];
+        int result = 0;
 
-        N = Integer.parseInt(br.readLine());
-        int tc = 1;
-        while (N != 0){
-            arr = new int[N][N];
-
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                for (int j = 0; j < N; j++) {
-                    arr[i][j] = Integer.parseInt(st.nextToken());
+        Arrays.fill(minEdge, Integer.MAX_VALUE);
+        minEdge[start] = 0;
+        for (int i = 0; i < N; i++) {
+            int minVertex = -1;
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < N; j++) {
+                if (!v[j] && min > minEdge[j]){
+                    minVertex = j;
+                    min = minEdge[j];
                 }
             }
 
+            if (minVertex == -1) return -1;
+            v[minVertex] = true;
+            result += min;
+            if (minVertex == end) break;
 
-            sb.append("Problem ").append(tc++).append(": ").append(arr[N-1][N-1]).append("\n");
-            N = Integer.parseInt(br.readLine());
+            for(int[] edge : g[minVertex]){
+                if (!v[edge[0]] && minEdge[edge[0]] > min + edge[1]){
+                    minEdge[edge[0]] = min + edge[1];
+                }
+            }
         }
-        System.out.println(sb.toString());
+        if (minEdge[end] == Integer.MAX_VALUE) return -1;
+        return minEdge[end];
     }
 
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        g = new ArrayList[N];
+
+        for (int i = 0; i < N; i++)
+            g[i] = new ArrayList<>();
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken()) - 1;
+            int weight = Integer.parseInt(st.nextToken());
+            g[from].add(new int[] {to, weight});
+            g[to].add(new int[] {from, weight});
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        v1 = Integer.parseInt(st.nextToken()) - 1;
+        v2 = Integer.parseInt(st.nextToken()) - 1;
+
+        int min = Integer.MAX_VALUE;
+
+        int[] d1 = new int[3];
+        int[] d2 = new int[3];
+
+        d1[0] = dijkstra(0, v1);
+        d1[1] = dijkstra(v1, v2);
+        d1[2] = dijkstra(v2, N - 1);
+
+        d2[0] = dijkstra(0, v2);
+        d2[1] = dijkstra(v2, v1);
+        d2[2] = dijkstra(v1, N - 1);
+
+
+        if (!(d1[0] == -1 ||d1[1] == -1 ||d1[2] == -1))
+            min = (min < d1[0] + d1[1] + d1[2]) ? min : d1[0] + d1[1] + d1[2];
+        if (!(d2[0] == -1 ||d2[1] == -1 ||d2[2] == -1))
+            min = (min < d2[0] + d2[1] + d2[2]) ? min : d2[0] + d2[1] + d2[2];
+
+        if (min == Integer.MAX_VALUE)
+            System.out.println(-1);
+        else
+            System.out.println(min);
+    }
+//    (1 -> v1 -> v2 -> N)
 }
