976c3a605ef4cc736b6806161a398601dc0a47e9
slcloe <gkstmf1403@naver.com>
2023-09-13 23:05:47 +0900

    bj 1932

--- hanseul/src/math/week8_bj_1629_곱셈.java
+++ hanseul/src/math/week8_bj_1629_곱셈.java
@@ -1,11 +1,18 @@
-package implementation;
+package math;
 
 import java.io.*;
 import java.util.*;
 
 public class week8_bj_1629_곱셈 {
-
-
+    static long recurMuliply(long a, int b, int c){
+        if (b == 1) return a % c;
+        long mul = (recurMuliply(a, b / 2, c) % c);
+        mul = (mul * mul) % c;
+        if (b % 2 == 0)
+            return (mul);
+        else
+            return (mul * a) % c;
+    }
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -14,10 +21,7 @@ public class week8_bj_1629_곱셈 {
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < B; i++) {
-            A = (A * A) % C;
-        }
-        System.out.println(A);
+        System.out.println(recurMuliply(A, B ,C) % C);
     }
 
 }
