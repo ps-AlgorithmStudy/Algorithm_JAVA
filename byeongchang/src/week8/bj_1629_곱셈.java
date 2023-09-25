fd047d8c13ce99ba6e5bf10028a53350c337770b
dig04214 <dig04214@naver.com>
2023-09-25 17:59:49 +0900

    0925

---
+++ byeongchang/src/week8/bj_1629_곱셈.java
@@ -0,0 +1,35 @@
+package week8;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_1629_곱셈 {
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("res/bj_1269.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        //for(int tc=1; tc<=10; tc++)
+        System.out.println(solution(br,st));
+        br.close();
+    }
+    public static long solution(BufferedReader br, StringTokenizer st) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        return mod(A,B,C);
+    }
+    public static long mod(int a, int b, int c){
+        if(b==1) return a%c;
+        long tmp = mod(a,b/2,c);
+        if(b%2==0){
+            return tmp*tmp%c;
+        }
+        else {
+            return tmp*tmp%c*a%c;
+        }
+    }
+}
