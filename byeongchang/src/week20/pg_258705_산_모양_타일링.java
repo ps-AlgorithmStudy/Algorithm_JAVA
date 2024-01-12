a3478ea0ee8e05e25d9779685efe0cf44766ce5f
dig04214 <dig04214@naver.com>
2024-01-12 10:24:37 +0900

    문제 링크 추가

--- byeongchang/src/week20/pg_258705_산_모양_타일링.java
+++ byeongchang/src/week20/pg_258705_산_모양_타일링.java
@@ -1,5 +1,6 @@
 package week20;
 
+// https://school.programmers.co.kr/learn/courses/30/lessons/258705
 public class pg_258705_산_모양_타일링 {
     static final int TRAPEZOID = 0;    // 사다리꼴
     static final int MOD = 10007;    // 나누기 연산을 위한 값
