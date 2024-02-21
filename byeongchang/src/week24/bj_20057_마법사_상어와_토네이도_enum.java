42af44b7a6872a8908d004f5624d9b41b927c45e
dig04214 <dig04214@naver.com>
2024-02-21 10:19:19 +0900

    week 24 마법사 상어와 토네이도 enum 풀이 최적화

--- byeongchang/src/week24/bj_20057_마법사_상어와_토네이도_enum.java
+++ byeongchang/src/week24/bj_20057_마법사_상어와_토네이도_enum.java
@@ -32,9 +32,11 @@ public class bj_20057_마법사_상어와_토네이도_enum {
         int answer = 0;
         int idx = 1;
         Direction[] dir = Direction.values();
+        SpreadSand[] sv = SpreadSand.values();
         int N = map.length;
         int x = N / 2;
         int y = N / 2;
+        Sand sn = new Sand(x,y,0,0, map);
         while (x > 0 || y > 0) {
             int direction = idx % 2 == 1 ? 0 : 1;    // idx가 홀수이면 진행방향이 좌,하, idx가 짝수이면 진행방향이 우,상
             for (int i = 2 * direction; i <= 2 * direction + 1; i++) {
@@ -46,13 +48,15 @@ public class bj_20057_마법사_상어와_토네이도_enum {
                     if (nx < 0 || ny < 0)
                         return answer;    // 토네이도가 0,0이면 소멸
 
+                    sn.change(nx, ny, dx, dy);    // 객체 재사용해서 생성하는 시간 줄임
+                    map[nx][ny] = 0;
+
                     // 좌표 별 이동하는 모래 계산하기
-                    answer += transferSand(nx, ny, dx, dy, map);
+                    answer += transferSand(sn, sv);
 
                     x = nx;
                     y = ny;
                 }
-
             }
             idx++;
         }
@@ -63,17 +67,13 @@ public class bj_20057_마법사_상어와_토네이도_enum {
 
 
     // 좌표 별 이동하는 모래 계산
-    public static int transferSand(int nx, int ny, int dx, int dy, int[][] map) {
+    public static int transferSand(Sand sn, SpreadSand[] sv) {
         int result = 0;
-        int sand = map[nx][ny];
-        int left = sand;
-        map[nx][ny] = 0;
 
         // enum을 이용한 반복문
-        for(SpreadSand s: SpreadSand.values()) {
-            int[] res = s.calc.apply(new Sand(nx, ny, dx, dy, sand, map.length, left, map));
-            left -= res[0];
-            result += res[1];
+        for(SpreadSand ss: sv) {
+            int res = ss.calc.apply(sn);
+            result += res;
         }
         return result;
     }
@@ -108,31 +108,31 @@ public class bj_20057_마법사_상어와_토네이도_enum {
             int ax = n.nx + (2 * n.dx);
             int ay = n.ny + (2 * n.dy);
             int dv = (int) (n.sand * 0.05);
-            return new int[]{dv, checkBorder(dv, ax, ay, n.N, n.map)};
+            return checkBorder(dv, ax, ay, n.N, n.map);
         }),
         B((n)->{
             int ax = n.nx + n.dy;
             int ay = n.ny + -1 * n.dx;
             int dv = (int) (n.sand * 0.07);
-            return new int[]{dv, checkBorder(dv, ax, ay, n.N, n.map)};
+            return checkBorder(dv, ax, ay, n.N, n.map);
         }),
         C((n)->{
             int ax = n.nx + 2 * n.dy;
             int ay = n.ny + -2 * n.dx;
             int dv = (int) (n.sand * 0.02);
-            return new int[]{dv, checkBorder(dv, ax, ay, n.N, n.map)};
+            return checkBorder(dv, ax, ay, n.N, n.map);
         }),
         D((n)->{
             int ax = n.nx + -1 * n.dy;
             int ay = n.ny + n.dx;
             int dv = (int) (n.sand * 0.07);
-            return new int[]{dv, checkBorder(dv, ax, ay, n.N, n.map)};
+            return checkBorder(dv, ax, ay, n.N, n.map);
         }),
         E((n)->{
             int ax = n.nx + -2 * n.dy;
             int ay = n.ny + 2 * n.dx;
             int dv = (int) (n.sand * 0.02);
-            return new int[]{dv, checkBorder(dv, ax, ay, n.N, n.map)};
+            return checkBorder(dv, ax, ay, n.N, n.map);
         }),
         F((n)->{
             int ax, ay;
@@ -145,7 +145,7 @@ public class bj_20057_마법사_상어와_토네이도_enum {
                 ay = n.ny + -1 * n.dx;
             }
             int dv = (int) (n.sand * 0.1);
-            return new int[]{dv, checkBorder(dv, ax, ay, n.N, n.map)};
+            return checkBorder(dv, ax, ay, n.N, n.map);
         }),
         G((n)->{
             int ax, ay;
@@ -158,7 +158,7 @@ public class bj_20057_마법사_상어와_토네이도_enum {
                 ay = n.ny + n.dx;
             }
             int dv = (int) (n.sand * 0.1);
-            return new int[]{dv, checkBorder(dv, ax, ay, n.N, n.map)};
+            return checkBorder(dv, ax, ay, n.N, n.map);
         }),
         H((n)->{
             int ax, ay;
@@ -170,7 +170,7 @@ public class bj_20057_마법사_상어와_토네이도_enum {
                 ay = n.ny - n.dx;
             }
             int dv = (int) (n.sand * 0.01);
-            return new int[]{dv, checkBorder(dv, ax, ay, n.N, n.map)};
+            return checkBorder(dv, ax, ay, n.N, n.map);
         }),
         I((n)->{
             int ax, ay;
@@ -182,19 +182,20 @@ public class bj_20057_마법사_상어와_토네이도_enum {
                 ay = n.ny + n.dx;
             }
             int dv = (int) (n.sand * 0.01);
-            return new int[]{dv, checkBorder(dv, ax, ay, n.N, n.map)};
+            return checkBorder(dv, ax, ay, n.N, n.map);
         }),
         J((n)->{
             int ax = n.nx + n.dx;
             int ay = n.ny + n.dy;
-            return new int[]{n.left, checkBorder(n.left, ax, ay, n.N, n.map)};
+            int dv = n.sand -((int) (n.sand * 0.05) + (int) (n.sand * 0.07) * 2 + (int) (n.sand * 0.02) * 2 + (int) (n.sand * 0.1) * 2 + (int) (n.sand * 0.01) * 2);
+            return checkBorder(dv, ax, ay, n.N, n.map);
         });
 
 
 
-        final Function<Sand, int[]> calc;
+        final Function<Sand, Integer> calc;
 
-        SpreadSand(Function<Sand, int[]> func) {
+        SpreadSand(Function<Sand, Integer> func) {
             this.calc = func;
         }
 
@@ -208,18 +209,27 @@ public class bj_20057_마법사_상어와_토네이도_enum {
         int dy;
         int sand;
         int N;
-        int left;
         int[][] map;
 
-        public Sand(int nx, int ny, int dx, int dy, int sand, int n, int left, int[][] map) {
+        public Sand(int nx, int ny, int dx, int dy, int sand, int n, int[][] map) {
             this.nx = nx;
             this.ny = ny;
             this.dx = dx;
             this.dy = dy;
             this.sand = sand;
             this.N = n;
-            this.left = left;
             this.map = map;
+        }
+        public Sand(int nx, int ny, int dx, int dy, int[][] map) {
+            this(nx, ny, dx, dy, map[nx][ny], map.length, map);
+        }
+
+        void change(int nx, int ny, int dx, int dy) {
+            this.nx = nx;
+            this.ny = ny;
+            this.dx = dx;
+            this.dy = dy;
+            this.sand = map[nx][ny];
         }
     }
 }
